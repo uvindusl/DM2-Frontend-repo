2180223ffe18df750aa93be7d81fa945006cfda6
HarinduVihan <harinduvihan@gmail.com>
2025-04-18 00:10:53 +0530

    reviews

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SupplierLoginPage from "./pages/SupplierLoginPage";
 import SupplierDashboard from "./pages/SupplierDashborad";
 import SupplierViewFood from "./pages/SupplierViewFood";
 import CustomerProfile from "./pages/CustomerProfile";
+import CustomerReview from "./pages/CustomerReview";
 
 function App() {
   return (
@@ -59,6 +60,7 @@ function App() {
           <Route path="/supplier/dashboard" element={<SupplierDashboard />} />
           <Route path="/supplier/view/foods" element={<SupplierViewFood />} />
           <Route path="/customer/profile" element={<CustomerProfile />} />
+          <Route path="/customer/reviews" element={<CustomerReview />} />
         </Routes>
       </main>
     </div>
