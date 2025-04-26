6b5c3ea348c8e25cf153da4d3c6fa6921b6432e1
HarinduVihan <harinduvihan@gmail.com>
2025-04-26 12:58:49 +0530

    deleted title

--- src/components/MostSoldProducts.tsx
+++ src/components/MostSoldProducts.tsx
@@ -6,7 +6,6 @@ function MostSoldProducts() {
       <h2 className="Most-Sold-Products-title">Most Sold Products</h2>
       <div className="Most-Sold-Products-grid">
         <div className="Most-Sold-Product-card">
-          <h2 className="Most-Sold-Products-title">1st Place</h2>
           <div className="Most-Sold-Products-details">
             <img src="#" alt="Product 1" className="Most-Sold-Products-image" />
             <p className="Most-Sold-Products-name">Name 1</p>
@@ -14,7 +13,6 @@ function MostSoldProducts() {
           </div>
         </div>
         <div className="Most-Sold-Product-card">
-          <h2 className="Most-Sold-Products-title">2nd Place</h2>
           <div className="Most-Sold-Products-details">
             <img src="#" alt="Product 1" className="Most-Sold-Products-image" />
             <p className="Most-Sold-Products-name">Name 2</p>
@@ -22,7 +20,6 @@ function MostSoldProducts() {
           </div>
         </div>
         <div className="Most-Sold-Product-card">
-          <h2 className="Most-Sold-Products-title">3rd Place</h2>
           <div className="Most-Sold-Products-details">
             <img src="#" alt="Product 1" className="Most-Sold-Products-image" />
             <p className="Most-Sold-Products-name">Name 3</p>
